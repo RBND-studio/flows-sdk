d1d360f93b62c9fe03e6d781359d7e7e638e2661
Vojtěch Vidra <vojtechvidra@gmail.com>
2025-01-01 16:02:24 +0100

    chore: delete old js package

--- workspaces/js/src/lib/step-type.ts
+++
@@ -1,9 +0,0 @@
-import type { FlowStep, FlowTooltipStep, FlowModalStep, FlowBannerStep } from "../types";
-
-export const isTooltipStep = (step: FlowStep): step is FlowTooltipStep =>
-  "targetElement" in step && Boolean(step.targetElement);
-export const isBannerStep = (step: FlowStep): step is FlowBannerStep =>
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition -- remove this ignore after we add type to other step types
-  "type" in step && step.type === "banner";
-export const isModalStep = (step: FlowStep): step is FlowModalStep =>
-  !isTooltipStep(step) && ("title" in step || "body" in step) && !isBannerStep(step);
