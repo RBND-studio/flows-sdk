04c06108dc4a81584b982682ea3c43a21796b97f
Vojtěch Vidra <vojtechvidra@gmail.com>
2025-02-04 10:47:09 +0100

    fix(react): use current pathname when rendering (#290)

--- workspaces/react/src/contexts/pathname-context.tsx
+++ workspaces/react/src/contexts/pathname-context.tsx
@@ -11,7 +11,7 @@ import {
   type ReactNode,
 } from "react";
 
-const INTERVAL_MS = 250;
+const INTERVAL_MS = 200;
 
 const _usePathname = (): string | undefined => {
   const [pathname, setPathname] = useState<string>();
@@ -38,7 +38,12 @@ const _usePathname = (): string | undefined => {
 
 type IPathnameContext = string | undefined;
 const PathnameContext = createContext<IPathnameContext>(undefined);
-export const usePathname = (): IPathnameContext => useContext(PathnameContext);
+export const usePathname = (): IPathnameContext => {
+  const contextValue = useContext(PathnameContext);
+  // Avoid accessing window on the server
+  if (contextValue === undefined) return;
+  return getPathname();
+};
 
 interface Props {
   children?: ReactNode;
