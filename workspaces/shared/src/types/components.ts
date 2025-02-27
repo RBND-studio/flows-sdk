711fbfba875a5adf627b5d37337a0ab6ebef6ff0
Vojtěch Vidra <vojtechvidra@gmail.com>
2025-02-27 11:50:27 +0100

    feat: type documentation

--- workspaces/shared/src/types/components.ts
+++ workspaces/shared/src/types/components.ts
@@ -1,15 +1,28 @@
 /* eslint-disable @typescript-eslint/no-explicit-any -- needed for loose types */
 
+/**
+ * Properties provided by Flows based on block and block template setup.
+ */
 export interface FlowsProperties {
+  /**
+   * User defined key for identifying the block.
+   */
   key?: string;
 }
 
 export type ComponentProps<T extends Record<string, any> = any> = {
+  /**
+   * Properties provided by Flows based on block and block template setup.
+   */
   __flows: FlowsProperties;
 } & T;
 
 export type TourComponentProps<T extends Record<string, any> = any> = {
+  /**
+   * Properties provided by Flows based on block and block template setup.
+   */
   __flows: FlowsProperties;
+
   continue: () => void;
   previous?: () => void;
   cancel: () => void;
