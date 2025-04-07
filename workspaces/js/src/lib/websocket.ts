0ba72d8c842547f500d823b7597365420d6fd379
Vojtěch Vidra <23375601+VojtechVidra@users.noreply.github.com>
2025-04-07 15:51:48 +0200

    feat(js,react): disconnect usage limited users (#356)
    * feat(js,react): disconnect usage limited users
    
    * fix: websocket test in firefox

--- workspaces/js/src/lib/websocket.ts
+++ workspaces/js/src/lib/websocket.ts
@@ -4,19 +4,37 @@ interface Props {
   onOpen: () => void;
 }
 
+export type Disconnect = () => void;
+
 // TODO: implement better automatic reconnection
-export const websocket = (props: Props): void => {
-  const connect = (): void => {
+export const websocket = (props: Props): { disconnect: Disconnect } => {
+  const connect = (): { disconnect: Disconnect } => {
     const socket = new WebSocket(props.url);
 
-    socket.addEventListener("message", props.onMessage);
-    socket.addEventListener("open", props.onOpen);
-    socket.addEventListener("close", () => {
+    const handleClose = (): void => {
       setTimeout(() => {
         connect();
       }, 2000);
-    });
+    };
+
+    socket.addEventListener("message", props.onMessage);
+    socket.addEventListener("open", props.onOpen);
+    socket.addEventListener("close", handleClose);
+
+    const disconnect = (): void => {
+      socket.removeEventListener("message", props.onMessage);
+      socket.removeEventListener("open", props.onOpen);
+      socket.removeEventListener("close", handleClose);
+
+      if (socket.readyState === WebSocket.CONNECTING) {
+        socket.addEventListener("open", () => {
+          socket.close();
+        });
+      } else socket.close();
+    };
+
+    return { disconnect };
   };
 
-  connect();
+  return connect();
 };
