d90ad6e54c253301d0e3c08c3a749a8d65c1bbeb
Vojtěch Vidra <23375601+VojtechVidra@users.noreply.github.com>
2025-11-14 09:23:00 +0100

    fix(js-components): incorrect component type (#498)

--- workspaces/js-components/src/types/components.ts
+++ workspaces/js-components/src/types/components.ts
@@ -1,7 +1,5 @@
-import { type LitElement } from "lit";
-
-export type Component = typeof LitElement;
-export type TourComponent = typeof LitElement;
+export type Component = CustomElementConstructor;
+export type TourComponent = CustomElementConstructor;
 
 export type Components = Record<string, Component>;
 export type TourComponents = Record<string, TourComponent>;
