b4aaf90cd077603ca7163bc2260a24a1bb7b6721
Vojtěch Vidra <23375601+VojtechVidra@users.noreply.github.com>
2025-11-20 10:29:47 +0100

    docs: update JS CDN example to components V2 (#508)
    * docs: update JS CDN example to components V2
    
    * docs: update readme
    
    * docs: fix banner

--- examples/js/cdn/flows.js
+++ examples/js/cdn/flows.js
@@ -1,40 +1,19 @@
 // @flows/js
-const { init, addFloatingBlocksChangeListener, addSlotBlocksChangeListener } = flows_js;
+const {
+  init,
+  addFloatingBlocksChangeListener,
+  addSlotBlocksChangeListener,
+  getCurrentFloatingBlocks,
+  getCurrentSlotBlocks,
+} = flows_js;
 
 // @flows/js-components
-const { render, updateSlotComponents } = flows_js_components;
-const providedComponents = flows_js_components_components;
-const providedTourComponents = flows_js_components_tour_components;
+const { setupJsComponents } = flows_js_components;
+const components = flows_js_components_components;
+const tourComponents = flows_js_components_tour_components;
 
-/**
- * Custom Card component definition
- * @param {{ text: string, continue: () => void }} props
- * @returns {{ element: HTMLElement, cleanup: () => void }}
- */
-const Card = (props) => {
-  const card = document.createElement("div");
-
-  const text = document.createElement("p");
-  text.innerText = props.text;
-  card.appendChild(text);
-
-  const closeButton = document.createElement("button");
-  closeButton.innerText = "Close";
-  closeButton.addEventListener("click", props.close);
-  card.appendChild(closeButton);
-
-  return {
-    element: card,
-    cleanup: () => {
-      closeButton.removeEventListener("click", props.close);
-    },
-  };
-};
-
-// Define your components, we're using pre-built components and a custom Card component
-const components = { ...providedComponents, Card };
-// We're not providing custom components for the tour but you can add any components you want.
-const tourComponents = { ...providedTourComponents };
+import { Banner } from "./banner.js";
+import { TourBanner } from "./tour-banner.js";
 
 // Initialize the SDK with your options
 init({
@@ -42,13 +21,21 @@ init({
   userId: "YOUR_USER_ID",
   environment: "production",
 });
-// Setup <flows-slot>
-updateSlotComponents({
-  components,
-  tourComponents,
+setupJsComponents({
+  components: {
+    ...components,
+    // Example of custom "Banner" component
+    Banner,
+  },
+  tourComponents: {
+    ...tourComponents,
+    // Example of custom "Banner" component for tours
+    Banner: TourBanner,
+  },
+
+  // We need to pass @flows/js methods to fix non working cross package imports in CDN setup
+  addFloatingBlocksChangeListener,
   addSlotBlocksChangeListener,
-});
-// Setup rendering of floating blocks
-addFloatingBlocksChangeListener((blocks) => {
-  render({ blocks, components, tourComponents });
+  getCurrentFloatingBlocks,
+  getCurrentSlotBlocks,
 });
