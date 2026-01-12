a268452697934d6f71f14bd0121317a6da2df190
Vojtěch Vidra <23375601+VojtechVidra@users.noreply.github.com>
2026-01-12 09:47:55 +0100

    feat(react,react-components): support for LinkComponent prop

--- workspaces/react/src/types/components.ts
+++ workspaces/react/src/types/components.ts
@@ -1,8 +1,15 @@
 import { type ComponentProps, type TourComponentProps } from "@flows/shared";
-import { type FC } from "react";
+import { type ReactNode, type FC } from "react";
 
 export type Component = FC<ComponentProps>;
 export type Components = Record<string, Component>;
 
 export type TourComponent = FC<TourComponentProps>;
 export type TourComponents = Record<string, TourComponent>;
+
+export interface LinkComponentProps {
+  href: string;
+  children: ReactNode;
+  className?: string;
+  onClick?: () => void;
+}
