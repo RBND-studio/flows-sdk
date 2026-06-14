df2b2d455260f5eb05e44fa7e22f6fa89694474a
Vojtěch Vidra <23375601+VojtechVidra@users.noreply.github.com>
2026-06-14 12:43:17 +0200

    docs: update react vite example to use React Router (#701)
    * docs: update react vite example to use React Router
    
    * fix: readme
    
    * docs: replace cover
    
    ---------
    
    Co-authored-by: Ondřej Pešička <77627332+OPesicka@users.noreply.github.com>

---
+++ examples/react/vite/app/flows.tsx
@@ -0,0 +1,43 @@
+import type { FC, ReactNode } from "react";
+import { Link } from "react-router";
+
+import { FlowsProvider, type LinkComponentType } from "@flows/react";
+import * as components from "@flows/react-components";
+import * as tourComponents from "@flows/react-components/tour";
+import * as surveyComponents from "@flows/react-components/survey";
+import "@flows/react-components/index.css";
+
+import { Banner } from "~/components/banner";
+import { TourBanner } from "~/components/tour-banner";
+
+type Props = {
+  children: ReactNode;
+};
+
+const LinkComponent: LinkComponentType = ({ href, children, className, onClick }) => (
+  <Link to={href} className={className} onClick={onClick}>
+    {children}
+  </Link>
+);
+
+export const Flows: FC<Props> = ({ children }) => {
+  return (
+    <FlowsProvider
+      organizationId="YOUR_ORGANIZATION_ID"
+      userId="YOUR_USER_ID"
+      environment="production"
+      LinkComponent={LinkComponent}
+      components={{
+        ...components,
+        Banner: Banner,
+      }}
+      tourComponents={{
+        ...tourComponents,
+        Banner: TourBanner,
+      }}
+      surveyComponents={{ ...surveyComponents }}
+    >
+      {children}
+    </FlowsProvider>
+  );
+};
