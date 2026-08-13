abc85c1757bfd48c88f29b44dc672f1ac665169f
Vojtěch Vidra <23375601+VojtechVidra@users.noreply.github.com>
2026-08-13 09:55:21 +0200

    feat(react): refetch data when language changes

--- workspaces/react/src/hooks/use-blocks.ts
+++ workspaces/react/src/hooks/use-blocks.ts
@@ -136,7 +136,7 @@ export const useBlocks = ({
       });
   }, [apiUrl, language, params, customFetch, onAfterLoad]);
 
-  // Refetch blocks when userProperties change
+  // Refetch blocks when userProperties or language change
   const fetchBlocksRef = useRef(fetchBlocks);
   fetchBlocksRef.current = fetchBlocks;
   const firstRenderRef = useRef(true);
@@ -146,7 +146,7 @@ export const useBlocks = ({
       return;
     }
     fetchBlocksRef.current();
-  }, [userProperties]);
+  }, [userProperties, language]);
 
   const websocketUrl = useMemo(() => {
     if (usageLimited) return;
